db884e56799ccc0ff27ca06f39ddc3a87475c134
AUNGMYATLWIN <zack.cloud7@gmail.com>
2023-03-02 20:51:31 +0630

    update

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,22 @@
+import { useState } from 'react';
+import { createUser } from '../util/auth';
 import AuthContent from '../components/Auth/AuthContent';
+import LoadingOverlay from  '../components/ui/LoadingOverlay';
 
 function SignupScreen() {
-  return <AuthContent />;
+  const [isAuthenticating, setIsAuthenticating]=useState(false);
+  
+  async function signupHandler({email, password}){
+    setIsAuthenticating(true);
+    await createUser(email, password);
+    setIsAuthenticating(false);
+  }
+
+  if(isAuthenticating){
+    return <LoadingOverlay message="Creating User...." />;
+  }
+
+  return <AuthContent onAuthenticate={signupHandler}/>;
 }
 
 export default SignupScreen;
