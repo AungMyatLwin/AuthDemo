e7683e7dcd9dc502407479760f40111069bac79c
AUNGMYATLWIN <zack.cloud7@gmail.com>
2023-03-02 22:02:04 +0630

    removed api key

--- util/auth.js
+++ util/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY='AIzaSyBfER2ALDR2trtNaLnojTmCN7D3cXw4hhg';
+
 
 async function authenticate(mode, email, password){
     const url=`https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
