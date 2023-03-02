2c13d9fe019663ec4a3952289307fedb3e0434dd
AUNGMYATLWIN <zack.cloud7@gmail.com>
2023-03-02 21:13:34 +0630

    error handling

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Alert } from 'react-native';
 import AuthContent from '../components/Auth/AuthContent';
 import LoadingOverlay from '../components/ui/LoadingOverlay';
 import {login} from '../util/auth';
@@ -8,7 +9,11 @@ function LoginScreen() {
   
   async function loginHandler({email, password}){
     setIsAuthenticating(true);
+   try {
     await login(email, password);
+   } catch (error) {
+      Alert.alert('Authetication failed!',`${error}`);
+   }
     setIsAuthenticating(false);
   }
 
