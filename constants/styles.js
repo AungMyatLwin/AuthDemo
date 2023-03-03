3a68773469b80d0fafa03a7a85a06e3bc2b401a8
AUNGMYATLWIN <zack.cloud7@gmail.com>
2023-03-03 18:21:29 +0630

    fira santorini color palette.

--- constants/styles.js
+++ constants/styles.js
@@ -1,7 +1,10 @@
 export const Colors = {
-  primary100: '#f9beda',
-  primary500: '#c30b64',
-  primary800: '#610440',
-  error100: '#fcdcbf',
-  error500: '#f37c13',
+  primary100: '#81c8ee',
+  primary500: '#006bbd',
+  primary800: '#284277',
+  error100: '#ede8e4',
+  error500: '#c1afa8',
 }
+
+// #2a9cd0 #b0deeb #e6e4e5 #bfbdbe #807f85
+// #284277 #006bbd #81c8ee #ede8e4 #c1afa8
